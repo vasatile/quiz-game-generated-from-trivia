045157bb330f96f0790c5573ee3810621efbec4c
vasatileucee <vasatileucee@gmail.com>
2023-06-24 12:33:55 +0100

    Initial commit

---
+++ quiz_brain.py
@@ -0,0 +1,31 @@
+class QuizBrain :
+    def __init__(self, a_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = a_list
+
+
+    def still_has_question(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            False
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"{self.question_number}: {current_question.text}(True/False): " )
+        self.check_answer(user_answer, current_question.answer)
+
+
+    def check_answer(self, user_answer, correct_answer):
+
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("you got it right")
+
+        else:
+            print("thats wrong.")
+        print(f" the correct answer is: {correct_answer}")
+        print(f"your current score is {self.score}/{self.question_number}")
+        print("\n")
